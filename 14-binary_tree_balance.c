f2c413edd21795c9699662aea57b9d350d35ac09
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-11-30 16:17:56 +0100

    feat: First commit for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,5 +4,8 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
+		return (-1);
+
+
+	return  (binary_tree_balance(tree->left) - binary_tree_balance(tree->right));
 }
