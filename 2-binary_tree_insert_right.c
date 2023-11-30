bb4924a189f37f97012a08c81c8cf6255c49d5d2
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-11-30 15:08:18 +0100

    fix: Fix checker error with task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,21 +5,22 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
+	if (!parent)
+		return (NULL);
 	newNode = malloc(sizeof(binary_tree_t));
 	if (!newNode || !parent)
 		return (NULL);
 
 	newNode->n = value;
+	newNode->right = NULL;
 	newNode->left = NULL;
 	newNode->parent = parent;
 
-	if (parent->right != NULL)
+	if (parent->right)
 	{
 		newNode->right = parent->right;
 		parent->right->parent = newNode;
 	}
-	else
-		newNode->right = NULL;
 	parent->right = newNode;
 
 	return (newNode);
